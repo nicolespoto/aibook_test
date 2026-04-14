3fae46337960adaa93757a52fbe03b190c4e0af9
Nicole Spoto <46899268+nicolespoto@users.noreply.github.com>
2026-04-14 11:48:23 -0400

    Add files via upload

---
+++ Sequential Word Processing RNN Concept Practice.py
@@ -0,0 +1,20 @@
+# Programming Exercise 8-1: Sequential Word Processing
+# Complete the missing parts where indicated.
+
+# TODO: Ask the user for a sentence
+sentence = ________________________________
+
+# TODO: Split the sentence into individual words
+words = ________________________________
+
+# This variable will store the running "memory"
+memory = ""
+
+# TODO: Loop through the words one at a time
+for i in range(________________):
+    
+    # TODO: Update the memory by adding the current word
+    memory = __________________________________________
+    
+    # TODO: Print the step number and the current memory
+    print(_____________________________________________)
